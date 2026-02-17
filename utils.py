bb33d188384addd3f3a673df2ba3d59554e5e5a7
Dasha <dashahrihorieva@knu.ua>
2026-02-17 21:25:43 +0200

    Initial commit: added utils.py and main.py

--- utils.py
+++ utils.py
@@ -8,3 +8,13 @@ def gcd(a, b):
     while b:
         a, b = b, a % b
     return a
+def get_fibonacci(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+
+    a, b = 0, 1
+    for _ in range(2, n + 1):
+        a, b = b, a + b
+    return b
