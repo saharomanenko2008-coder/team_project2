be14e4880fb2dd15cd8948b531724037946edd3d
saharomanenko2008-coder <saharomanenko2008@gmail.com>
2026-02-22 13:58:45 +0200

    Add is_power_of_five function to main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from utils import factorial, gcd, get_fibonacci
+from utils import factorial, gcd, get_fibonacci,is_power_of_five
 print("Factorial of 5:", factorial(5))
 print(gcd(24, 18))
 print(get_fibonacci(5))
+print(is_power_of_five(5))
